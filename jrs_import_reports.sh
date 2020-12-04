8d94429e1d1436787d60066de59168a73613ff68
Lev Buymistryuk <catinwarmhands@users.noreply.github.com>
2020-12-04 14:57:28 +0400

    fix encoding

--- jrs_import_reports.sh
+++ jrs_import_reports.sh
@@ -1,9 +1,9 @@
-REPORTS_DIR=${1:-JR_templates} # РѕРїС†РёРѕРЅР°Р»СЊРЅРѕ РјРѕР¶РЅРѕ Р·Р°РґР°С‚СЊ РїР°РїРєСѓ СЃ Р°СЂС…РёРІР°РјРё РґР»СЏ РёРјРїРѕСЂС‚Р°. РџРѕ СѓРјРѕР»С‡Р°РЅРёСЋ СЌС‚Рѕ JR_templates
+REPORTS_DIR=${1:-JR_templates} # опционально можно задать папку с архивами для импорта. По умолчанию это JR_templates
 JRS_DIR=/opt/jasper/buildomatic
 
 SCRIPT_DIR=$(
-    cd $(dirname "$0")
-    pwd
+	cd $(dirname "$0")
+	pwd
 )
 LOG_DIR=$SCRIPT_DIR/log
 LOG_FILE=$LOG_DIR/JRS_import.log
@@ -14,23 +14,23 @@ mkdir -p $LOG_DIR
 rm -f $LOG_FILE
 
 for f in $SCRIPT_DIR/$REPORTS_DIR/*.zip; do
-    if [ -e $f ]; then # РїСЂРѕРІРµСЂРєР°, С‡С‚Рѕ С„Р°Р№Р» РЅР°Р№РґРµРЅ
-        printf "Importing \"$f\"..."
-        cd $JRS_DIR
-        echo "---------------- Importing $f ----------------" &>> $LOG_FILE
-        sh js-import.sh --broken-dependencies include --update --skip-user-update --input-zip $f &>> $LOG_FILE
-        if [ $? -eq 0 ]; then # РїСЂРѕРІРµСЂРєР°, С‡С‚Рѕ РёРјРїРѕСЂС‚ РїСЂРѕС€С‘Р» СѓСЃРїРµС€РЅРѕ
-            echo OK
-        else
-            echo FAIL
-            echo Logs written at $LOG_FILE
-            exit 1
-        fi
-        cd $SCRIPT_DIR
-    else
-        echo Files not found!
-        exit 1
-    fi
+	if [ -e $f ]; then # проверка, что файл найден
+		printf "Importing \"$f\"..."
+		cd $JRS_DIR
+		echo "---------------- Importing $f ----------------" &>> $LOG_FILE
+		sh js-import.sh --broken-dependencies include --update --skip-user-update --input-zip $f &>> $LOG_FILE
+		if [ $? -eq 0 ]; then # проверка, что импорт прошёл успешно
+			echo OK
+		else
+			echo FAIL
+			echo Logs written at $LOG_FILE
+			exit 1
+		fi
+		cd $SCRIPT_DIR
+	else
+		echo Files not found!
+		exit 1
+	fi
 done
 
 echo Done!
